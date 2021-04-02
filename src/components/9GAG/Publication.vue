31714c2a9a791f77be0ac1982fd5e7268c3231c0
Magali LY SAO <magali.ly-sao@informatique.gov.pf>
2021-04-01 16:18:37 -1000

    add moderation

--- src/components/9GAG/Publication.vue
+++ src/components/9GAG/Publication.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="card mb-5 p-5" v-if="login">
+    <p v-if="utilisateur.role === 'MODERATEUR'" class="alert-danger text-center">Vous êtes le modérateur</p>
     <h2 class="text-md-left">9GAG</h2>
     <router-link to="Publication">Ajouter une publication</router-link>
     <hr>
@@ -14,6 +15,10 @@
           </div>
           <div class="mt-2">
             <img class="img-thumbnail" :src="value.image_url">
+          </div>
+          <div id="moderateur" v-if="!value.modere && utilisateur.role === 'MODERATEUR'">
+            <div class="btn btn-danger mt-2" @click="valider(value.id)">Valider la publication</div>
+            <p class="text-danger mt-2" v-if="errorMsg">{{ errorMsg }}</p>
           </div>
         </div>
         <hr>
@@ -33,6 +38,8 @@ export default {
     return {
       publications: [],
       login: !!sessionStorage.getItem('token'),
+      utilisateur: JSON.parse(sessionStorage.getItem('user')),
+      errorMsg: ''
     }
   },
   props: ['liste'],
@@ -43,6 +50,22 @@ export default {
           this.publications = reponse.data;
         })
   },
+  methods: {
+    valider: function (id){
+      this.$http
+          .put(`http://localhost:3000/api/publications/${id}`, {
+            publication: {
+              modere: true
+            }
+          })
+          .then(() => {
+            location.reload();
+          })
+          .catch(error => {
+            this.errorMsg = error.response.data.error;
+          })
+    }
+  }
 }
 </script>
 
